9d64198c2ff9d59fe3d1300e801957d80fab8d00
Pierre-Yves Gerardy <py@py.gy>
2016-01-04 22:33:58 +0100

    Renamed the project

--- compose-regexp.js
+++ compose-regexp.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-    typeof define === 'function' && define.amd ? define('sane-regexp', ['exports'], factory) :
-    (factory((global.saneRegexp = {})));
+    typeof define === 'function' && define.amd ? define('compose-regexp', ['exports'], factory) :
+    (factory((global.composeRegexp = {})));
 }(this, function (exports) { 'use strict';
 
     var empty = new RegExp('')
