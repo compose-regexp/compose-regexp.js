48b1ac3d5f9e6ed83c4aaba4d2d20c12b74d780d
Pierre-Yves <py@py.gy>
2022-05-05 14:24:28 +0200

    Simplify the release process

--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -10,9 +10,6 @@ const {version} = pkg
 const messages = []
 
 
-if (!process.env.NPM_PUBLISH) messages.push("/!\\ Please use `npm run release`")
-
-
 try { await readNpm('run', 'test')} catch({stderr, stdout}) {
     messages.push("/!\\ There was a problem with the test suite", stdout, stderr)
 }
@@ -24,7 +21,7 @@ if (gitOutput.trim() !== '') messages.push("/!\\ The git working tree is not cle
 
 
 const {stdout: branch} = await readGit('branch', '--show-current')
-if (branch !== "main\n") messages.push(`/!\\ We are on branch ${branch.trim()}, we release from main`)    
+if (branch !== "main\n") messages.push(`/!\\ We are on branch ${branch.trim()}, we publish from main`)    
 
 
 const {stdout: remoteVersion} = await readNpm("view", "compose-regexp", "version")
